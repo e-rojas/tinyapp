988ec6304f6f437c2c7364815f88abba0c264c3f
javascript01fan <ejrojas@icloud.com>
2019-09-02 16:40:35 -0400

    urls_index added

--- express_server.js
+++ express_server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = 4000      //default port
-
-const urlDatabases ={
+app.set('view engine','ejs')
+const urlDatabase ={
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"  
 }
-app.get('/',(request,response)=>{
-    response.send('Hello')
+
+app.get('/urls',(request,response)=>{
+    let templateVaars = {urls:urlDatabase}
+    response.render('urls_index',templateVaars)
 })
-app.get('/urls.json',(request,response)=>{
-    response.json(urlDatabases)
-})
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on port:${PORT}`)
