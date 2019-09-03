937fe114eb1250571f6597cc20acefdd409cabcc
javascript01fan <ejrojas@icloud.com>
2019-09-03 15:37:10 -0400

    delete buttons added and fuctioning

--- express_server.js
+++ express_server.js
@@ -9,28 +9,33 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/urls", (request, response) => {
+app.get("/urls", (req, res) => {
   let templateVaars = { urls: urlDatabase };
-  response.render("urls_index", templateVaars);
+  res.render("urls_index", templateVaars);
 });
-app.get("/urls/new", (request, response) => {
-  response.render("urls_new");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
-app.get("/urls/:shortURL", (request, response) => {
-  console.log(request.params.shortURL);
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
   let templateVars = {
-    shortURL: request.params.shortURL,
-    longURL: urlDatabase[request.params.shortURL]
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
   };
-  response.render("urls_show", templateVars);
+  res.render("urls_show", templateVars);
 });
-app.post("/urls", (request, response) => {
-  console.log(request.body); //// Log the POST request body to the console
-  response.send("OK"); // Respond with 'Ok' (we will replace this)
+app.post("/urls", (req, res) => {
+    urlDatabase[generateRandomString()]= req.body.longURL
+  console.log(req.body.longURL); //// Log the POST req body to the console
+  res.send("OK"); // Respond with 'Ok' (we will replace this)
 });
-
+//Deleting short urls
+app.post('/urls/:shortURL/delete',(req,res)=>{
+delete urlDatabase[req.params.shortURL]
+res.redirect('/urls')
+})
 const generateRandomString = ()=>{
-   return Math.random().toString(36).slice(7)
+   return Math.random().toString(36).substring(7)
 }
 console.log(generateRandomString())
 
